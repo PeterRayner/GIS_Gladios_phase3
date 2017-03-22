fd74ece37a97e6ddf380367290aaf5272fbe2036
ShockwaveZA <minal.pramlall@yahoo.com>
2017-03-22 19:38:56 +0200

    Update GIS.java

--- GIS_Implementation/GIS.java
+++ GIS_Implementation/GIS.java
@@ -23,6 +23,10 @@ public class GIS {
         
     }
     
+    /**
+     * Constructor for the class that will initialize the fields and establish connection
+     * to the database with the default parameters.
+     */
     private GIS(){
     
             Connection c = null;
@@ -43,6 +47,14 @@ public class GIS {
     
     }
     
+    /**
+     * Constructor for the class that will initialize the fields and establish connection
+     * to the database.
+     *
+     * @param db        String value that holds the name of the database
+     * @param password  String value that holds the password which will authenticate the
+     *                  connection
+     */
     private GIS(String db, String password){
 
         this.dbName=db;
@@ -53,25 +65,50 @@ public class GIS {
     
     }
     
+    /**  
+     * Creates a new table in the database, provided that the authenticated user has
+     * the appropriate permissions.
+     *
+     * @param table     A string value of the object to be deleted
+     * @return          A boolean value on whether the method was successful or not
+     */
     private boolean newTable(String table){
     //creates new table from string
     
     }
     
-     private boolean insert(String values){
+    /**  
+     * Inserts an item into the database, provided that the authenticated user has the
+     * appropriate permissions.
+     *
+     * @param values    A string value of the object to be inserted
+     * @return          A boolean value on whether the method was successful or not
+     */
+    private boolean insert(String values){
     //inserts values into database
     
     }
      
-     
-      private boolean update(String values){
+    /**  
+     * Updates an item from the database, provided that the authenticated user has the
+     * appropriate permissions.
+     *
+     * @param values    A string value of the object to be updated
+     * @return          A boolean value on whether the method was successful or not
+     */ 
+    private boolean update(String values){
     //updates value with given values string
     
     }
       
-      
-      
-      private boolean delete(String values){
+    /**  
+     * Removes an item from the database, provided that the authenticated user has the
+     * appropriate permissions.
+     *
+     * @param values    A string value of the object to be deleted
+     * @return          A boolean value on whether the method was successful or not
+     */
+    private boolean delete(String values){
     //deletes item on the database
     
     }
