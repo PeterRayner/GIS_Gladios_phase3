4cb42159e5c2f2b5f356c4105ca35dd6f8eb7e58
Peter Rayner <peter.rayner3@gmail.com>
2017-03-22 22:04:42 +0200

    function name updates
    update of function names as specified in navup specification

--- GIS_Implementation/GIS.java
+++ GIS_Implementation/GIS.java
@@ -13,6 +13,10 @@ public class GIS {
         
     }
     
+    /**
+     * Constructor for the class that will initialize the fields and establish connection
+     * to the database with the default parameters.
+     */
     private GIS(){
         
      System.out.println("Opened database successfully");
@@ -21,13 +25,22 @@ public class GIS {
     
     
     
+    /**
+     * Constructor for the class that will initialize the fields and establish connection
+     * to the database.
+     *
+     * @param db        String value that holds the name of the database
+     * @param password  String value that holds the password which will authenticate the
+     *                  connection
+     */
     private GIS(String db, String password){
 
      System.out.println("Opened database successfully");
     
     }
     
-    private float[] getCoordinates(String b){
+
+  private float[] getCoordinates(String b){
         float[] temp={1,14};
         return temp;
     } 
@@ -39,25 +52,56 @@ public class GIS {
         
     }
     
+    
+    public String getGISDataObject(String a){ return a;}
+     public String modifyGISData(String a){ return a;}
+
+  
+   /**  
+     * Creates a new table in the database, provided that the authenticated user has
+     * the appropriate permissions.
+     *
+     * @param table     A string value of the object to be deleted
+     * @return          A boolean value on whether the method was successful or not
+     */
     private boolean newTable(String table){
     //creates new table from string
         return true;
     }
     
-     private boolean insert(String values){
+    /**  
+     * Inserts an item into the database, provided that the authenticated user has the
+     * appropriate permissions.
+     *
+     * @param values    A string value of the object to be inserted
+     * @return          A boolean value on whether the method was successful or not
+     */
+    private boolean insert(String values){
     //inserts values into database
         return true;
     }
      
-     
-      private boolean update(String values){
+    /**  
+     * Updates an item from the database, provided that the authenticated user has the
+     * appropriate permissions.
+     *
+     * @param values    A string value of the object to be updated
+     * @return          A boolean value on whether the method was successful or not
+     */ 
+    
+    private boolean update(String values){
     //updates value with given values string
         return true;
     }
       
-      
-      
-      private boolean delete(String values){
+    /**  
+     * Removes an item from the database, provided that the authenticated user has the
+     * appropriate permissions.
+     *
+     * @param values    A string value of the object to be deleted
+     * @return          A boolean value on whether the method was successful or not
+     */
+    private boolean delete(String values){
     //deletes item on the database
         return true;
     }
