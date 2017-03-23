ca4e42b9b23ebd93d883be4472b4d1a10ab18339
Mandla Mashinini <u29630135@tuks.co.za>
2017-03-23 15:16:57 +0200

    Added to the Main function

--- GIS_Implementation/GIS.java
+++ GIS_Implementation/GIS.java
@@ -1,15 +1,26 @@
 
-package gis;
+//package gis;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class GIS {
 
 
-    
     public static void main(String[] args) {
         // TODO code application logic here
         // do all your testing of your modules here
+        GIS gis = new GIS();
+        GIS g = new GIS("mongo", "gisDep");
+
+        gis.getCoordinates("LAB300");
+        String loc = g.getLocation(4.42f, 2.44f);
+        System.out.println(loc);
+        gis.getGISDataObject("GISdataObject");
+        g.modifyGISData("ModifiedGIS");
+        gis.newTable("Issa New Table");
+        g.update("Update");
+        gis.insert("Going In!");
+        g.delete("To the Abyss");
         
     }
     
@@ -68,9 +79,9 @@ public class GIS {
     }
 
     
-    
+
     public String getGISDataObject(String a){ return a;}
-     public String modifyGISData(String a){ return a;}
+    public String modifyGISData(String a){ return a;}
 
 
   
