ebfaf8917d33d6cc78a4aea5a60011db0c07afaf
Peter Rayner <peter.rayner3@gmail.com>
2017-04-01 22:38:49 +0200

    Update GIS.java
    update for connection

--- GIS_Implementation/GIS.java
+++ GIS_Implementation/GIS.java
@@ -1,6 +1,11 @@
 package gis;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*; 
+import java.util.*; 
+import java.lang.*; 
+import static java.sql.DriverManager.getConnection;
+import org.postgis.*; 
+
+
 
 public class GIS implements GISInterface {
   
@@ -10,6 +15,7 @@ public class GIS implements GISInterface {
           
         
     }
+
     
     /**
      * Constructor for the class that will initialize the fields and establish connection
@@ -30,21 +36,76 @@ public class GIS implements GISInterface {
      */
     public GIS(String db, String password){
 
+            java.sql.Connection conn; 
+
+  try { 
+    
+    Class.forName("org.postgresql.Driver"); 
+    String url = "jdbc:postgresql://localhost:5433/postgis_23_sample"; 
+    
+    // DriverManager.getConnection(url, username, password);
+    conn = DriverManager.getConnection(url, "postgres", "1234"); 
+    /* 
+    * Add the geometry types to the connection. Note that you 
+    * must cast the connection to the pgsql-specific connection 
+    * implementation before calling the addDataType() method. 
+    */
+    ((org.postgresql.Connection)conn).addDataType("geometry","org.postgis.PGgeometry");
+   // ((org.postgresql.Connection)conn).addDataType("box3d","org.postgis.PGbox3d");
+    /* 
+    * Create a statement and execute a select query. 
+    */ 
+   // PreparedStatement ps = conn.prepareStatement("CREATE TABLE IF NOT EXISTS buildings(");
+   // ps.executeUpdate();
+   // ps.close();
+    
+    
+    // ps = conn.prepareStatement("CREATE TABLE IF NOT EXISTS lectureHalls");
+   // ps.executeUpdate();
+   // ps.close();
+    
+    
+  // Statement s = conn.createStatement(); 
+   // ResultSet r = s.executeQuery("select AsText(geom) as geom,id from geomtable"); 
+   // while( r.next() ) { 
+      /* 
+      * Retrieve the geometry as an object then cast it to the geometry type. 
+      * Print things out. 
+      */ 
+    //  PGgeometry geom = (PGgeometry)r.getObject(1); 
+     // int id = r.getInt(2); 
+     // System.out.println("Row " + id + ":");
+     // System.out.println(geom.toString()); 
+   // } 
+   // s.close(); 
+   // conn.close(); 
+  } 
+catch( Exception e ) { 
+  e.printStackTrace(); 
+  } 
+
+
      System.out.println("Opened database successfully");
     
     }
-    
+
+
+
+
     /**
      * Queries the database and returns the coordinates for the respective building location
      *
      * @param b     name of the building
      * @return      array of coordinates for the requested building
      */
-    private float[] getCoordinates(String b){
+    @Override
+    public float[] getCoordinates(String b){
+
         float[] temp={1,14};
         return temp;
     } 
     
+
     /**
      * Queries the database and returns the building location for the respective coordinates
      * 
@@ -52,18 +113,20 @@ public class GIS implements GISInterface {
      * @param y     Y coordinate of the building
      * @return      name of the building
      */
-    private String getLocation( float x, float y )
+    @Override
+    public String getLocation( float x, float y )
     {
         
         return "IT 4-4";
         
     }
-    
 
+
+    @Override
     public String getGISDataObject(String a){ return a;}
+    @Override
     public String modifyGISData(String a){ return a;}
 
-  
    /**  
      * Creates a new table in the database, provided that the authenticated user has
      * the appropriate permissions.
@@ -71,6 +134,7 @@ public class GIS implements GISInterface {
      * @param table     A string value of the object to be deleted
      * @return          A boolean value on whether the method was successful or not
      */
+    @Override
     public boolean newTable(String table){
     //creates new table from string
         return true;
@@ -83,6 +147,7 @@ public class GIS implements GISInterface {
      * @param values    A string value of the object to be inserted
      * @return          A boolean value on whether the method was successful or not
      */
+    @Override
     public boolean insert(String values){
     //inserts values into database
         return true;
@@ -96,6 +161,7 @@ public class GIS implements GISInterface {
      * @return          A boolean value on whether the method was successful or not
      */ 
     
+    @Override
     public boolean update(String values){
     //updates value with given values string
         return true;
@@ -108,6 +174,7 @@ public class GIS implements GISInterface {
      * @param values    A string value of the object to be deleted
      * @return          A boolean value on whether the method was successful or not
      */
+    @Override
     public boolean delete(String values){
     //deletes item on the database
         return true;
