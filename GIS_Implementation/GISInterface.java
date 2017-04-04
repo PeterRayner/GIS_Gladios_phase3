47f7f85cb19a2361b3ecedd427312efd3001d48d
Peter Rayner <peter.rayner3@gmail.com>
2017-04-04 15:35:11 +0200

    updated functions and comments
    added new functions for demo

--- GIS_Implementation/GISInterface.java
+++ GIS_Implementation/GISInterface.java
@@ -6,10 +6,29 @@
 package gis;
 
 public interface GISInterface{
+  //--------------------------------------------working-------------------------------------------------------------//
+    //takes in the building name and gives back all the classroom names in an array of strings 
+     public String[] getClassRooms(String b);// THIS WORKS
+     // returns the names of all the buildings in an array of strings
+    public String[] getAllBuildings(); //THIS WORKS
+    //takes in the name of a room and returns the coordinates in a float array 
+    // takes a string such as IT 4-4 and returns the coordinates in a double array
+     public double[] getLocationTemp(String b); // THIS WORKS
   
-    public float[] getCoordinates(String b);
-     public String getLocation( float x, float y );
-     public String getGISDataObject(String a);
+  
+    //-----------------------------------------uderwork------------------------------------------------------------//
+    public float[] getCoordinates(String b); // implemented- needs testing
+    
+    // takes in the longitude and latitude and returns the place which x and y lay inside
+     public String getLocation( float x, float y );  // implemented- needs testing
+    
+  
+      public String[] getAllLocations() ; // implemented- needs testing
+      public String[] locationsWithinRadius(float x, float y, float r); // implemented- needs testing
+   
+  
+  // will return a JSON object of the building that is passed through.
+   public String getGISDataObject(String a);
       public String modifyGISData(String a);
       public boolean newTable(String table);
       public boolean insert(String values);
