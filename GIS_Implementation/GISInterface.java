6b809240018f100108c795798e63363fe2c9af5b
Peter Rayner <peter.rayner3@gmail.com>
2017-04-20 17:49:41 +0200

    remade interface for GIS class
    all working functions made for GIS class

--- GIS_Implementation/GISInterface.java
+++ GIS_Implementation/GISInterface.java
@@ -1,42 +1,27 @@
 
+import java.util.ArrayList;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gis;
+public interface GISInterface {
 
-public interface GISInterface{
-  //--------------------------------------------working-------------------------------------------------------------//
-    //takes in the building name and gives back all the classroom names in an array of strings 
-     public String[] getClassRooms(String b);// THIS WORKS
-     // returns the names of all the buildings in an array of strings
-    public String[] getAllBuildings(); //THIS WORKS
-    //takes in the name of a room and returns the coordinates in a float array 
-    // takes a string such as IT 4-4 and returns the coordinates in a double array
-     public double[] getLocationTemp(String b); // THIS WORKS
-  
-  
-    //-----------------------------------------uderwork------------------------------------------------------------//
-    public float[] getCoordinates(String b); // implemented- needs testing
-    
-    // takes in the longitude and latitude and returns the place which x and y lay inside
-     public String getLocation( float x, float y );  // implemented- needs testing
-    
-  
-      public String[] getAllLocations() ; // implemented- needs testing
-      public String[] locationsWithinRadius(float x, float y, float r); // implemented- needs testing
-   
-  
-  // will return a JSON object of the building that is passed through.
-   public String getGISDataObject(String a);
-      public String modifyGISData(String a);
-      public boolean newTable(String table);
-      public boolean insert(String values);
-       public boolean update(String values);
-       public boolean delete(String values);
+    public void createMap();
 
+    public String getBuilding(String name);
+
+    public ArrayList<String> getAllBuildings();
+
+    ArrayList<String> getLectureHall(String building);
+
+    public ArrayList<Double> getLectureCoordinates(String room);
+
+    public ArrayList<Double> getBuildingCoordinates(String building);
+
+    public ArrayList<String> getBuildingInRadius(double mLat, double mLon, double radius);
+
+    public void insertBuilding(String name, String description, String geometry, String coordinates, String table);
 }
-    
-
-
